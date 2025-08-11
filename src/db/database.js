a1e0a17c62d51c4bb1b64ca77a9e5d5d182f9b4e
KeinPC <kein.carrillo.escom@gmail.com>
2025-08-10 23:51:11 -0600

    db

---
+++ src/db/database.js
@@ -0,0 +1,14 @@
+import { Sequelize } from 'sequelize'
+
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: 5432
+  }
+)
+
+export default sequelize

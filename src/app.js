43e84d51bb16c9aa3f2b254ce67c1d8a2eb9f1f3
KeinPC <kein.carrillo.escom@gmail.com>
2025-08-10 23:52:00 -0600

    api

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import morgan from 'morgan'
+import pkg from '../package.json'
+
+// Routes
+import productsRoutes from './routes/products.routes.js'
+
+const app = express()
+
+// Settings
+app.set('pkg', pkg)
+
+// Middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({
+    name: app.get('pkg').name,
+    author: app.get('pkg').author,
+    description: app.get('pkg').description,
+    version: app.get('pkg').version
+  })
+})
+
+// Routes
+app.use('/products', productsRoutes)
+
+export default app

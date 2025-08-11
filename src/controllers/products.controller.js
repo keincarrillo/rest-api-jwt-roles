f1fd0d5bfd0a12983626ca1d5daa97fe17678bcc
KeinPC <kein.carrillo.escom@gmail.com>
2025-08-11 00:36:51 -0600

    add methods: PUT, DELETE, GET(product)

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -25,13 +25,55 @@ export const createProduct = async (req, res) => {
 }
 
 export const updateProduct = async (req, res) => {
-  res.send('update product')
+  const { name, category, price, imgURL } = req.body
+  const { productId } = req.params
+  try {
+    const [rowsAfected, [productUpdated]] = await Product.update(
+      {
+        name,
+        category,
+        price,
+        imgURL
+      },
+      { where: { id: productId }, returning: true }
+    )
+    rowsAfected === 0
+      ? res.status(404).json({
+          message: `No se encontro el producto con el id: ${productId}`
+        })
+      : res.json(productUpdated)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({ message: error.message })
+  }
 }
 
 export const deleteProduct = async (req, res) => {
-  res.send('delete product')
+  const { productId } = req.params
+  try {
+    const productDeleted = await Product.destroy({ where: { id: productId } })
+    productDeleted === 0
+      ? res.status(404).json({
+          message: `No se encontro el producto con el id: ${productId}`
+        })
+      : res.sendStatus(204)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({ message: error.message })
+  }
 }
 
 export const getProduct = async (req, res) => {
-  res.send('get product')
+  const { productId } = req.params
+  try {
+    const product = await Product.findByPk(productId)
+    product === null
+      ? res.status(404).json({
+          message: `No se encontro el producto con el id: ${productId}`
+        })
+      : res.json(product)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({ message: error.message })
+  }
 }

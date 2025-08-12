09fe8f13d2c3f28d91ab67b03bd30739196e1e4b
KeinPC <kein.carrillo.escom@gmail.com>
2025-08-11 18:14:07 -0600

    refactor code db

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 import app from './src/app.js'
-import sequelize from './src/db/database.js'
-
-// Relations
-import './src/models/User_Roles.js'
 
 const port = process.env.PORT || 3000
-
-try {
-  await sequelize.sync({ force: true })
-} catch (error) {
-  console.error(error)
-}
 
 app.listen(port, () => {
   console.log(`Server on port ${port}`)
--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import express from 'express'
 import morgan from 'morgan'
 import pkg from '../package.json'
+import sequelize from './db/database.js'
+import { createRoles } from './libs/initialSetup.js'
+
+// Relations
+import './models/User_Roles.js'
 
 // Routes
 import productsRoutes from './routes/products.routes.js'
 import authRoutes from './routes/auth.routes.js'
+
+// Database && initial setup
+try {
+  await sequelize.sync({ force: true })
+  await createRoles()
+} catch (error) {
+  console.error(error)
+}
 
 const app = express()
 
